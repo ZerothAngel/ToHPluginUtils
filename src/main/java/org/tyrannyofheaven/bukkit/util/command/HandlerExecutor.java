4f73fa9e214768394f7046741565e568ed7cff91
Allan Saddi <allan@saddi.com>
2011-09-01 21:53:00 -0700

    Fix up asserts. Add copyright & license.

--- src/main/java/org/tyrannyofheaven/bukkit/util/command/HandlerExecutor.java
+++ src/main/java/org/tyrannyofheaven/bukkit/util/command/HandlerExecutor.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011 Allan Saddi <allan@saddi.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.tyrannyofheaven.bukkit.util.command;
 
 import java.lang.annotation.Annotation;
@@ -227,7 +242,7 @@ public class HandlerExecutor<T extends Plugin> {
                     result.add(pa.getRest());
                 }
                 else {
-                    throw new AssertionError();
+                    throw new AssertionError("Unknown SpecialParameter type");
                 }
             }
             else if (mp instanceof OptionMetaData) {
@@ -293,7 +308,7 @@ public class HandlerExecutor<T extends Plugin> {
                 }
             }
             else {
-                throw new AssertionError();
+                throw new AssertionError("Unknown MethodParameter type");
             }
         }
         return result.toArray();
