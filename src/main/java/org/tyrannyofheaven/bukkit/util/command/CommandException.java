2f0f4ecf904b55a3572461f9430576f44683cd0a
Allan Saddi <allan@saddi.com>
2011-09-03 00:04:43 -0700

    Clean up exception handling.

--- src/main/java/org/tyrannyofheaven/bukkit/util/command/CommandException.java
+++ src/main/java/org/tyrannyofheaven/bukkit/util/command/CommandException.java
@@ -25,19 +25,11 @@ class CommandException extends RuntimeException {
 
     private static final long serialVersionUID = -4453503398619490556L;
 
-    public CommandException() {
+    protected CommandException() {
     }
 
     public CommandException(String message) {
         super(message);
-    }
-
-    public CommandException(Throwable cause) {
-        super(cause);
-    }
-
-    public CommandException(String message, Throwable cause) {
-        super(message, cause);
     }
 
 }
--- src/main/java/org/tyrannyofheaven/bukkit/util/command/ParseException.java
+++ src/main/java/org/tyrannyofheaven/bukkit/util/command/ParseException.java
@@ -16,8 +16,8 @@
 package org.tyrannyofheaven.bukkit.util.command;
 
 /**
- * Thrown when something is wrong with the command arguments. Should result in
- * an error message being shown to the user.
+ * Thrown when something is wrong with the command arguments. Results in
+ * an error message being shown to the user as well as the command usage.
  * 
  * @author asaddi
  */
@@ -28,16 +28,8 @@ public class ParseException extends CommandException {
     public ParseException() {
     }
 
-    public ParseException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
     public ParseException(String message) {
         super(message);
-    }
-
-    public ParseException(Throwable cause) {
-        super(cause);
     }
 
 }
