581fefa0f2529799eff4864b579b3af558f30273
ZerothAngel <zerothangel@tyrannyofheaven.org>
2011-09-06 02:33:02 -0700

    Add colorize() utility method.

--- src/main/java/org/tyrannyofheaven/bukkit/util/ToHUtils.java
+++ src/main/java/org/tyrannyofheaven/bukkit/util/ToHUtils.java
@@ -25,6 +25,7 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.logging.Level;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.Plugin;
 
@@ -219,6 +220,97 @@ public class ToHUtils {
     public static void assertFalse(boolean test) {
         if (test)
             throw new AssertionError();
+    }
+
+    // For colorize()
+    private static enum ColorizeState {
+        TEXT, COLOR_OPEN, COLOR_NAME, COLOR_CLOSE;
+    }
+
+    /**
+     * Scans text for color tokens and replaces with the appropriate color
+     * code. Might be more convenient than string concatenation? Tokens
+     * consist of the color name enclosed in curly-braces. Braces may be
+     * escaped by doubling.
+     * 
+     * @param text the text to colorize
+     * @return the colorized result
+     */
+    public static String colorize(String text) {
+        if (text == null) return null;
+        
+        StringBuilder out = new StringBuilder();
+
+        ColorizeState state = ColorizeState.TEXT;
+        StringBuilder color = null;
+
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+
+            if (state == ColorizeState.TEXT) {
+                if (c == '{') {
+                    state = ColorizeState.COLOR_OPEN;
+                }
+                else if (c == '}') {
+                    state = ColorizeState.COLOR_CLOSE;
+                }
+                else {
+                    out.append(c);
+                }
+            }
+            else if (state == ColorizeState.COLOR_OPEN) {
+                if (c == '{') { // Escaped bracket
+                    out.append('{');
+                    state = ColorizeState.TEXT;
+                }
+                else if (Character.isUpperCase(c)) {
+                    // First character of color name
+                    color = new StringBuilder();
+                    color.append(c);
+                    state = ColorizeState.COLOR_NAME;
+                }
+                else {
+                    // Invalid
+                    throw new IllegalArgumentException("Invalid color name");
+                }
+            }
+            else if (state == ColorizeState.COLOR_NAME) {
+                if (Character.isUpperCase(c) || c == '_') {
+                    color.append(c);
+                }
+                else if (c == '}') {
+                    ChatColor chatColor = ChatColor.valueOf(color.toString());
+                    out.append(chatColor);
+                    state = ColorizeState.TEXT;
+                }
+                else {
+                    // Invalid
+                    throw new IllegalArgumentException("Invalid color name");
+                }
+            }
+            else if (state == ColorizeState.COLOR_CLOSE) {
+                // Optional, but for sanity's sake, to keep brackets matched
+                if (c == '}') {
+                    out.append('}'); // Collapse to single bracket
+                }
+                else {
+                    out.append('}');
+                    out.append(c);
+                }
+                state = ColorizeState.TEXT;
+            }
+        }
+        
+        // End of string
+        if (state == ColorizeState.COLOR_CLOSE) {
+            out.append('}');
+        }
+        else if (state != ColorizeState.TEXT) {
+            // Was in the middle of color name
+            throw new IllegalArgumentException("Invalid color name");
+        }
+
+        return out.toString();
     }
 
 }
