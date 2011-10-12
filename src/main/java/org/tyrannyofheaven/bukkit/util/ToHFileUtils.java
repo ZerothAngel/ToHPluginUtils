abc3f7be9aff0cda6eaa8f8965caf7a84ecf34f0
Allan Saddi <allan@saddi.com>
2011-10-11 21:14:02 -0700

    Add AnnotatedYamlConfiguration, a YamlConfiguration subclass that allows comments on its root-level properties. Add utilities to make it easier to deal with new configuration.

--- src/main/java/org/tyrannyofheaven/bukkit/util/ToHFileUtils.java
+++ src/main/java/org/tyrannyofheaven/bukkit/util/ToHFileUtils.java
@@ -18,13 +18,21 @@ package org.tyrannyofheaven.bukkit.util;
 import static org.tyrannyofheaven.bukkit.util.ToHLoggingUtils.log;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 
+import org.bukkit.configuration.Configuration;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
+import org.tyrannyofheaven.bukkit.util.configuration.AnnotatedYamlConfiguration;
 
 /**
  * File utilities.
@@ -96,6 +104,129 @@ public class ToHFileUtils {
             log(plugin, Level.SEVERE, "Error copying %s to %s", resourceName, outFile, e);
             return false;
         }
+    }
+
+    /**
+     * Return the specified Configuration. The Configuration may pull its defaults
+     * from the given resource file. (Note: the resource is loaded relative to the
+     * plugin class.) The Configuration may also use comments read from the given
+     * comments file.
+     *  
+     * @param plugin the plugin
+     * @param configDir the parent directory of the config file
+     * @param configName the name of the config file
+     * @param mustExist set to true if the file must exist (will throw FileNotFoundException if config not present)
+     * @param defaultsName the path of the defaults resource, relative to the plugin class. May be <code>null</code>.
+     * @param commentsName the path of the comments resource, relative to the plugin class. May be <code>null</code>.
+     * @return the Configuration object, with defaults and comments appropriately set
+     * @throws FileNotFoundException if mustExist is true and the config file is not found
+     */
+    public static FileConfiguration getConfig(Plugin plugin, File configDir, String configName, boolean mustExist, String defaultsName, String commentsName) throws FileNotFoundException {
+        File configFile = new File(configDir, configName);
+
+        AnnotatedYamlConfiguration config = new AnnotatedYamlConfiguration();
+        try {
+            config.load(configFile);
+        }
+        catch (FileNotFoundException e) {
+            if (mustExist)
+                throw e;
+            // Otherwise, ignore...
+        }
+        catch (IOException e) {
+            ToHLoggingUtils.error(plugin, "Error reading configuration %s", configFile, e);
+        }
+        catch (InvalidConfigurationException e) {
+            ToHLoggingUtils.error(plugin, "Error parsing configuration %s", configFile, e);
+        }
+
+        // Set defaults if present
+        if (defaultsName != null) {
+            InputStream defaultsInput = plugin.getClass().getResourceAsStream(defaultsName);
+            if (defaultsInput != null) {
+                Configuration defaults = YamlConfiguration.loadConfiguration(defaultsInput);
+                config.setDefaults(defaults);
+            }
+        }
+
+        // Set root-level comments, if appropriate file is present
+        if (commentsName != null) {
+            InputStream commentsInput = plugin.getClass().getResourceAsStream(commentsName);
+            if (commentsInput != null) {
+                Configuration comments = YamlConfiguration.loadConfiguration(commentsInput);
+                Map<String, String> commentsMap = new HashMap<String, String>();
+                for (Map.Entry<String, Object> entry : comments.getValues(false).entrySet()) {
+                    commentsMap.put(entry.getKey(), entry.getValue().toString());
+                }
+                config.setComments(commentsMap);
+            }
+        }
+
+        return config;
+    }
+
+    /**
+     * Return the specified Configuration. The Configuration may pull its defaults
+     * from the given resource file. (Note: the resource is loaded relative to the
+     * plugin class.) The Configuration may also use comments read from the given
+     * comments file.
+     *  
+     * @param plugin the plugin
+     * @param configDir the parent directory of the config file
+     * @param configName the name of the config file
+     * @param defaultsName the path of the defaults resource, relative to the plugin class. May be <code>null</code>.
+     * @param commentsName the path of the comments resource, relative to the plugin class. May be <code>null</code>.
+     * @return the Configuration object, with defaults and comments appropriately set
+     */
+    public static FileConfiguration getConfig(Plugin plugin, File configDir, String configName, String defaultsName, String commentsName) {
+        try {
+            return getConfig(plugin, configDir, configName, false, defaultsName, commentsName);
+        }
+        catch (FileNotFoundException e) {
+            // Should never get here because we set mustExist to false
+            throw new AssertionError();
+        }
+    }
+
+    /**
+     * Fetch the plugin's standard Configuration (from <code>plugins/plugin-name/config.yml</code>)
+     * and set it up with the appropriate defaults and comments resources, if present.
+     * The defaults and comments resources are expected to be at config.yml and
+     * config-comments.yml respectively, in the same package as the plugin's class.
+     * 
+     * @param plugin the plugin
+     * @return the Configuration object, appropriately configured with defaults and comments
+     */
+    public static FileConfiguration getConfig(Plugin plugin) {
+        return getConfig(plugin, plugin.getDataFolder(), "config.yml", "config.yml", "config-comments.yml");
+    }
+
+    /**
+     * Attempt to save a FileConfiguration.
+     * 
+     * @param plugin the plugin
+     * @param config the FileConfiguration to save
+     * @param configDir the parent directory of the file
+     * @param configName the config filename
+     */
+    public static void saveConfig(Plugin plugin, FileConfiguration config, File configDir, String configName) {
+        File configFile = new File(configDir, configName);
+        try {
+            config.save(configFile);
+        }
+        catch (IOException e) {
+            ToHLoggingUtils.error(plugin, "Error saving configuration %s", configFile, e);
+        }
+    }
+
+    /**
+     * Save a FileConfiguration as the plugin's standard config.yml.
+     * 
+     * @param plugin the plugin
+     * @param config the FileConfiguration
+     */
+    public static void saveConfig(Plugin plugin, FileConfiguration config) {
+        saveConfig(plugin, config, plugin.getDataFolder(), "config.yml");
     }
 
 }
